a9003048b50bc563323d1a3735764c92c2323bb4
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-10-20 13:58:12 +0100

    installedand configured firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+let app = "";
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp( {
+  apiKey: "AIzaSyATWUU9K7X1aXKpf6z37700lGzFIvfj6as",
+  authDomain: "vue-auth-system-ea23e.firebaseapp.com",
+  databaseURL: "https://vue-auth-system-ea23e.firebaseio.com",
+  projectId: "vue-auth-system-ea23e",
+  storageBucket: "vue-auth-system-ea23e.appspot.com",
+  messagingSenderId: "554038240117",
+  appId: "1:554038240117:web:5207c462aaef304d21e93c"
+});
+// Initialize Firebase
+
+firebase.auth().onAuthStateChanged(()=>{
+  if(!app){
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+
+  }
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,38 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:'*',
+    redirect:'/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/',
+    redirect:'/login'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    meta:{
+      requiresAuth:true
+    }
+  },
+  {
+    path:'/login',
+    name:'Login',
+    component:Login
+  },
+  {
+    path:'/sign-up',
+    name:'Signup',
+    component:Signup
   }
 ]
 
@@ -24,6 +40,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to,from,next)=>{
+  const currentUser = firebase.auth().currentUser
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if(requiresAuth && !currentUser) next('login');
+  else if(!requiresAuth && currentUser) next('home');
+  else next();
+});
 
 export default router
