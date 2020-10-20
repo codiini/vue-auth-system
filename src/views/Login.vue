abc19ca53d0532fab5d95568eda2c4bad23066dd
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2020-10-20 13:59:39 +0100

    Login component

---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email" v-model="email"/>
+    <input type="password" placeholder="Password" v-model="password" />
+    <button @click="login">Connection</button>
+    <p>You don't have an account? You can  <router-link to="/sign-up">create one</router-link></p>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: "Login",
+  data() {
+    return {
+      email:"",
+      password:""
+    };
+  },
+  methods: {
+    login(){
+       firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+          () => {
+            this.$router.replace('home')
+          },
+          (err) => {
+            alert('Oops. ' + err.message)
+          }
+        );
+    }
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
